7613beab9c65984d1370bfaf23857b7617908f4f
molnar95 <molnar.bence95@gmail.com>
2020-05-11 08:44:26 +0200

    Add functions

---
+++ R/currency1.R
@@ -0,0 +1,10 @@
+#' Currency Package
+#'
+#' Exchange currency
+#'
+#' @docType package
+#'
+#' @author Bence Molnar \email{molnar.bence95@gmail.com}
+#'
+#' @name currency
+NULL
---
+++ R/hello.R
@@ -0,0 +1,56 @@
+#' Convert selected currencies via API
+#'
+#' @param value numeric vector
+#' @param input character string
+#' @param output character string
+#'
+#' @return result
+#'
+#' @importFrom httr GET content
+#' @importFrom logger
+#'
+usd_rate <- function(value, input = 'EUR', output='USD') {
+  base <- GET(paste0('https://api.exchangeratesapi.io/latest?base=', input, '&symbols=', output))
+  base <- as.numeric(unlist(content(base)[[1]]))
+  result <- value * base
+  log_info('{value} {input} is equal to {result} {output}')
+  return(as.numeric(result))
+}
+
+#' EUR/USD converter
+#'
+#' @param val numeric vector
+#'
+#' @return value_eur numeric value
+#'
+#' @importFrom httr GET content
+#'
+convert_to_eur <- function(val){
+  value_eur <- usd_rate(val)
+  return(value_eur)
+}
+
+
+#' EUR pretty printer
+#'
+#' @param value numeric vector
+#'
+#' @return EUR character string
+#'
+eur <- function(value){
+  return(paste0('€', format(round(value, 2), nsmall=1, big.mark=",")))
+}
+
+#' EUR string to numeric converter
+#'
+#' @param str character string
+#'
+#' @return EUR in numeric format
+#'
+uneur <- function(str){
+  str <- gsub('€', '', str)
+  str <- gsub(',', '', str)
+  return(as.numeric(str))
+}
+
+
